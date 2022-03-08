2935adc530a32ddf29392d50d1d9ffb1730a8c92
SilverCOderZ <77063924+agriye@users.noreply.github.com>
2022-03-08 15:36:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+img = "";
+status = "";
+objects = [];
+function setup()
+{
+    canvas = createCanvas(380,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size();
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status - Detecting the objects!";
+
+}
+function modelLoaded()
+{
+    console.log("Model is Loaded!");
+    status = true;
+}
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+//!= means not equal to
+
+function draw()
+{
+    image(video,0,0,380,300);
+
+    if(status!="")
+{
+    r = random(255);
+    b = random(255);
+    g = random(255);
+    objectDetector.detect(video,gotResults);
+    for(i=0; i<objects.length; i++)
+    {
+        document.getElementById("status").innerHTML="Status : Objects Detected!";
+        document.getElementById("length").innerHTML = "Objects Detected - " + objects.length;
+        fill(r,g,b);
+        percent = floor(objects[i].confidence*100);
+        text(objects[i].label + " : " + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+    }
+   
+}
+    
+}
